342be60db56b2b5048867b923909a7b74a75404b
Alexandre Devassine <a.devassine@orange.fr>
2015-03-30 15:14:44 +0200

    testokun

---
+++ src/main/java/iut/tdd/Convert.java
@@ -0,0 +1,15 @@
+package iut.tdd;
+
+public class Convert {
+	public static String num2text(String input) {
+		if (input.equals("0")) {
+		return "zéro";
+		}
+		else { 
+			return "un"; 
+			}
+	}
+	public static String text2num(String input) {
+		return null;
+	}
+}
